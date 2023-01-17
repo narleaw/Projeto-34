1032ae74c9b6fb6e90a8cbfbf46217f12a17e326
narleaw <104634531+narleaw@users.noreply.github.com>
2023-01-17 08:35:22 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,11 +50,11 @@ function draw() {
 
 function blow() {
   
-   Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: -0.05 });
+//    Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: -0.05 });
   
   // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0.05, y: 0.05 });
   
-  //Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
+  Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: 0, y: 0.05 });
   
   // Matter.Body.applyForce(ball.body, { x: 0, y: 0 }, { x: -0.05, y: 0.05 });
 
